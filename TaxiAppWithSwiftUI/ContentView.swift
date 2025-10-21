dc4fb95e95c86e0517f551bbb960c3ede4ab1c48
shimiz-y <shimiz-y@sogonet.co.jp>
2025-10-21 17:25:34 +0900

    Initial Commit

---
+++ TaxiAppWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaxiAppWithSwiftUI
+//
+//  Created by shimizu  yusuke on 2025/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaxiAppWithSwiftUI/TaxiAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxiAppWithSwiftUIApp.swift
+//  TaxiAppWithSwiftUI
+//
+//  Created by shimizu  yusuke on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TaxiAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
