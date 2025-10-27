03d42f98ba5c0ec5741afb12e5a800835f6b481c
shimiz-y <shimiz-y@sogonet.co.jp>
2025-10-27 16:31:18 +0900

    Feat: 座標から地名を取得し表示するロジックを実装

--- TaxiAppWithSwiftUI/ViewModels/MainViewModel.swift
+++ TaxiAppWithSwiftUI/ViewModels/MainViewModel.swift
@@ -7,21 +7,36 @@
 
 import Foundation
 import MapKit
+import Combine
 
-class MainViewModel {
+class MainViewModel: ObservableObject {
     
-    let strPointName = "横浜市西区みなとみらい1-2"
+    @Published var strPointName = ""
+    
+    init () {
+
+        Task {
+            await getLocationAddress(latitude: 34.6894362, longitude: 135.4963352)
+        }
+    }
     
     func getLocationAddress(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async {
+
         let geocoder = CLGeocoder()
         let location = CLLocation(latitude: latitude, longitude: longitude)
         
         do {
             let placemarks = try await geocoder.reverseGeocodeLocation(location)
             
-            for placemark in placemarks {
-                print("placemark:\(placemark)")
-            }
+            guard let placemark = placemarks.first else { return }
+            
+            let administrativeArea = placemark.administrativeArea ?? ""
+            let locality = placemark.locality ?? ""
+            let subLocality = placemark.subLocality ?? ""
+            let throughfare = placemark.thoroughfare ?? ""
+            let subThroughfare = placemark.subThoroughfare ?? ""
+            
+            strPointName = "\(administrativeArea)\(locality)\(subLocality)\(throughfare)\(subThroughfare)"
             
         } catch {
             print("位置情報の処理に失敗：\(error.localizedDescription)")
