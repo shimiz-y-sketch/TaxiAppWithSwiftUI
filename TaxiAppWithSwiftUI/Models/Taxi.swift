b6cc28e6a20aaa0205be6fea53750734cd1dede7
shimiz-y <shimiz-y@sogonet.co.jp>
2025-11-07 13:30:36 +0900

    refactor: TaxiモデルをFirestoreのGeoPointに対応させDecodableに準拠
    * Taxi構造体にFirebaseのGeoPointプロパティを追加し、Decodableプロトコルに準拠させた。
    
    * GeoPointをCoreLocationのCLLocationCoordinate2Dに変換する計算プロパティ `coordinates` を定義した。
    
    * TaxiState enumにもDecodableへの準拠を追加した。
    
    * これにより、MainViewModelのfetchTaxis()メソッドで、Firestoreから取得したタクシーデータを正しくデコードし、Swiftオブジェクトとして利用する準備が完了した。

--- TaxiAppWithSwiftUI/Models/Taxi.swift
+++ TaxiAppWithSwiftUI/Models/Taxi.swift
@@ -7,19 +7,28 @@
 
 import Foundation
 import CoreLocation
+import FirebaseFirestore
 
-struct Taxi: Identifiable {
-        /// タクシーを一意に識別するためのID
-        let id: String
-        /// タクシーの車両番号
-        let number: String
-        /// タクシーの現在の緯度・経度座標
-        var coordinates: CLLocationCoordinate2D
-        /// タクシーの現在の状態（空車、乗車地へ移動中など）
-        var state: TaxiState
+struct Taxi: Identifiable, Decodable  {
+    /// タクシーを一意に識別するためのID
+    let id: String
+    /// タクシーの車両番号
+    let number: String
+    /// Firestoreから取得した緯度・経度情報を保持するプロパティ。
+    /// Decodableに準拠しているGeoPoint型として定義する。
+    var geoPoint: GeoPoint
+    /// タクシーの現在の状態（空車、乗車地へ移動中など）
+    var state: TaxiState
+    
+    /// タクシーの現在の緯度・経度座標
+    var coordinates: CLLocationCoordinate2D {
+        // GeoPointプロパティの値を利用して、MapKitやCoreLocationで利用可能な
+        // CLLocationCoordinate2D型へ変換し、返す計算プロパティ。
+        CLLocationCoordinate2D(latitude: geoPoint.latitude, longitude: geoPoint.longitude)
+    }
 }
 
-enum TaxiState {
+enum TaxiState: Decodable {
     // 空車状態
     case empty
     /// 乗車地へ向かっている状態
@@ -35,55 +44,55 @@ extension Taxi {
             Taxi(
                 id: "1",
                 number: "111-111",
-                coordinates: .init(latitude: 34.695301, longitude: 135.511417),
+                geoPoint: .init(latitude: 34.695301, longitude: 135.511417),
                 state: .empty
             ),
             Taxi(
                 id: "2",
                 number: "222-222",
-                coordinates: .init(latitude: 34.710001, longitude: 135.500417),
+                geoPoint: .init(latitude: 34.710001, longitude: 135.500417),
                 state: .empty
             ),
             Taxi(
                 id: "3",
                 number: "333-333",
-                coordinates: .init(latitude: 34.705301, longitude: 135.497417),
+                geoPoint: .init(latitude: 34.705301, longitude: 135.497417),
                 state: .goingToRidePoint
             ),
             Taxi(
                 id: "4",
                 number: "444-444",
-                coordinates: .init(latitude: 34.697301, longitude: 135.508417),
+                geoPoint: .init(latitude: 34.697301, longitude: 135.508417),
                 state: .empty
             ),
             Taxi(
                 id: "5",
                 number: "555-555",
-                coordinates: .init(latitude: 34.710301, longitude: 135.504417),
+                geoPoint: .init(latitude: 34.710301, longitude: 135.504417),
                 state: .goingToRidePoint
             ),
             Taxi(
                 id: "6",
                 number: "666-666",
-                coordinates: .init(latitude: 34.700301, longitude: 135.498417),
+                geoPoint: .init(latitude: 34.700301, longitude: 135.498417),
                 state: .goingToRidePoint
             ),
             Taxi(
                 id: "7",
                 number: "777-777",
-                coordinates: .init(latitude: 34.706301, longitude: 135.510417),
+                geoPoint: .init(latitude: 34.706301, longitude: 135.510417),
                 state: .empty
             ),
             Taxi(
                 id: "8",
                 number: "888-888",
-                coordinates: .init(latitude: 34.703301, longitude: 135.511417),
+                geoPoint: .init(latitude: 34.703301, longitude: 135.511417),
                 state: .goingToRidePoint
             ),
             Taxi(
                 id: "9",
                 number: "999-999",
-                coordinates: .init(latitude: 34.707301, longitude: 135.499417),
+                geoPoint: .init(latitude: 34.707301, longitude: 135.499417),
                 state: .empty
             )
         ]
